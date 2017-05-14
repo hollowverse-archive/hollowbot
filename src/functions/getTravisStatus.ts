64bcb2ef4a311a75b84346207c6ef569629c4eca
levsthings <bearnode@gmail.com>
2017-05-15 01:44:05 +0300

    add types, clean chains to async/await

--- src/functions/getTravisStatus.ts
+++ src/functions/getTravisStatus.ts
@@ -1,16 +1,16 @@
+import {Message} from 'discord.js'
 import {addressBook} from '../api/addressBook'
 import {getRequest} from '../api/externalCalls'
 
-export function getTravisStatus(message: any) {
-  getRequest(addressBook.travisBuild)
-    .then((res) => {
-      if (res.last_build_status === 0) {
-        message.reply(`**${res.slug}**'s most recent build on our CI is passing!`)
-      } else {
-        message.reply(`**${res.slug}**'s most recent build on our CI is failing!`)
-      }
-    })
-    .catch((err) => {
-      message.reply(err)
-    })
+export async function getTravisStatus(message: Message) {
+  try {
+    const res = await getRequest(addressBook.travisBuild)
+    if (res.last_build_status === 0) {
+      message.reply(`**${res.slug}**'s most recent build on our CI is passing!`)
+    } else {
+      message.reply(`**${res.slug}**'s most recent build on our CI is failing!`)
+    }
+  } catch (err) {
+    message.reply(err)
+  }
 }
