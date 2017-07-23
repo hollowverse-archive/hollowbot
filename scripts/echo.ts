201035bdfeaf802af41a66f94588a316e93c305c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-07-23 19:00:01 +0300

    Configure bot to use with Discord and Heroku

--- scripts/echo.ts
+++ scripts/echo.ts
@@ -1,7 +1,14 @@
 import * as hubot from 'hubot';
+import * as Chance from 'chance';
+
+const chance = new Chance();
 
 module.exports = (robot: hubot.Robot) => {
-  robot.hear(/echo (.+)$/, (res) => {
+  robot.hear(/echo (.+)$/, res => {
     res.reply(res.match[1]);
+  });
+
+  robot.respond(/flip a coin/, res => {
+    res.reply(chance.pickone(['head', 'tails']));
   });
 };
