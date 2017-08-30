67b3f6bc826f1f0bfed29cad11e2a949cb093049
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-30 20:08:07 +0300

    Use @hollowverse/common, set up new build and dev commands

--- src/echo.ts
+++ src/echo.ts
@@ -3,6 +3,10 @@ import * as Chance from 'chance';
 
 const chance = new Chance();
 
+const logger = (...args: any[]) => console.log(...args);
+
+logger(1);
+
 module.exports = (robot: hubot.Robot) => {
   robot.respond(/echo (.+)$/, res => {
     res.reply(res.match[1]);
