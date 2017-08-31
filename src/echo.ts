82ac8c2f4a7cd25579a06876f63e2dcd71a94fe6
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-31 13:36:51 +0300

    Clean up

--- src/echo.ts
+++ src/echo.ts
@@ -3,10 +3,6 @@ import * as Chance from 'chance';
 
 const chance = new Chance();
 
-const logger = (...args: any[]) => console.log(...args);
-
-logger(1);
-
 module.exports = (robot: hubot.Robot) => {
   robot.respond(/echo (.+)$/, res => {
     res.reply(res.match[1]);
