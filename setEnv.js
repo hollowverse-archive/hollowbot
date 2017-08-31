1c91e094287546e2238f9ff5ad3896cea7a57ce0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-08-31 15:59:43 +0300

    Add env file

---
+++ setEnv.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const SECRET_FILE_PATH = path.join('./secrets/discord.json');
+
+/**
+ * @type {DiscordSecret}
+ */
+const discordConfig = JSON.parse(fs.readFileSync(SECRET_FILE_PATH, 'utf8'));
+
+process.env.BOT_NAME = discordConfig.name;
+process.env.HUBOT_DISCORD_TOKEN = discordConfig.token;
