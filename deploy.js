587e635cce1187cc8a5e9ccb356c07e60fac80cc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-14 19:23:49 +0300

    Rewrite deploy script

--- deploy.js
+++ deploy.js
@@ -4,29 +4,12 @@
 const decryptSecrets = require('@hollowverse/common/helpers/decryptSecrets');
 const shelljs = require('shelljs');
 const executeCommands = require('@hollowverse/common/helpers/executeCommands');
-const retryCommand = require('@hollowverse/common/helpers/retryCommand');
-const writeEnvFile = require('@hollowverse/common/helpers/writeEnvFile');
 
-const {
-  ENC_PASS_HOLLOWBOT,
-  ENC_PASS_TRAVIS,
-  ENC_PASS_DISCORD,
-  PROJECT,
-  BRANCH,
-  IS_PULL_REQUEST,
-} = shelljs.env;
+const { ENC_PASS_DISCORD, IS_PULL_REQUEST } = shelljs.env;
 
 const isPullRequest = IS_PULL_REQUEST !== 'false';
 
 const secrets = [
-  {
-    password: ENC_PASS_HOLLOWBOT,
-    decryptedFilename: 'gcloud.hollowbot.json',
-  },
-  {
-    password: ENC_PASS_TRAVIS,
-    decryptedFilename: 'gcloud.travis.json',
-  },
   {
     password: ENC_PASS_DISCORD,
     decryptedFilename: 'discord.json',
@@ -34,34 +17,28 @@ const secrets = [
 ];
 
 async function main() {
-  let commands;
-  commands = ['yarn test'];
+  const buildCommands = ['yarn test', 'yarn server/build', 'yarn client/build'];
+  const deploymentCommands = [() => decryptSecrets(secrets, './secrets')];
 
-  if (BRANCH !== 'master') {
-    console.info('Skipping deployment on non-master branches');
-  } else if (isPullRequest === true) {
+  let isDeployment = false;
+  if (isPullRequest === true) {
     console.info('Skipping deployment commands in PRs');
+  } else if (secrets.some(secret => secret.password === undefined)) {
+    console.info(
+      'Skipping deployment commands because some secrets are not provided',
+    );
   } else {
-    commands = [
-      ...commands,
-      () => writeEnvFile('hollowbot', shelljs.env, './env.json'),
-      () => decryptSecrets(secrets, './secrets'),
-      `gcloud auth activate-service-account --key-file secrets/gcloud.travis.json`,
-      // Remove Travis key file so it does not get deployed with the service
-      () => {
-        shelljs.rm('./secrets/gcloud.travis.json*');
-        return 0;
-      },
-      () =>
-        retryCommand(
-          `gcloud app deploy app.yaml --project ${PROJECT} --version ${BRANCH} --quiet`,
-        ),
-    ];
+    isDeployment = true;
   }
 
-  const code = await executeCommands(commands);
-
-  process.exit(code);
+  try {
+    await executeCommands(
+      isDeployment ? [...buildCommands, ...deploymentCommands] : buildCommands,
+    );
+  } catch (e) {
+    console.error('Build/deployment failed:', e);
+    process.exit(1);
+  }
 }
 
 main();
